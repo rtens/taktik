f91f8aebe22e391ea15698f0a132209c4c5db530
Nikolas Martens <martens@jenke-institut.de>
2026-01-26 11:12:58 +0100

    Enter moves with numpad

---
+++ spec/plays_on_command_line/parses_plays_from_numpad.test.js
@@ -0,0 +1,43 @@
+import test from 'ava'
+import Move from '../../src/model/move.js'
+import parse from '../../src/model/parse.js'
+import Place from '../../src/model/place.js'
+
+test('place flat', t => {
+  t.deepEqual(parse('11'), Place.Flat.at(0, 0))
+  t.deepEqual(parse('21'), Place.Flat.at(1, 0))
+  t.deepEqual(parse('55'), Place.Flat.at(4, 4))
+})
+
+test('place wall', t => {
+  t.deepEqual(parse('/31'), Place.Wall.at(2, 0))
+  t.deepEqual(parse('/21'), Place.Wall.at(1, 0))
+  t.deepEqual(parse('/55'), Place.Wall.at(4, 4))
+})
+
+test('place cap', t => {
+  t.deepEqual(parse('*31'), Place.Cap.at(2, 0))
+  t.deepEqual(parse('*21'), Place.Cap.at(1, 0))
+  t.deepEqual(parse('*55'), Place.Cap.at(4, 4))
+})
+
+test('move one', t => {
+  t.deepEqual(parse('11.'), Move.at(0, 0).right().drop(1))
+  t.deepEqual(parse('22,'), Move.at(1, 1).left().drop(1))
+  t.deepEqual(parse('31-'), Move.at(2, 0).down().drop(1))
+  t.deepEqual(parse('33+'), Move.at(2, 2).up().drop(1))
+})
+
+test('move stack', t => {
+  t.deepEqual(parse('111.'), Move.at(0, 0).right().drop(1))
+  t.deepEqual(parse('221,'), Move.at(1, 0).left().drop(2))
+  t.deepEqual(parse('324+'), Move.at(1, 3).up().drop(3))
+  t.deepEqual(parse('422-'), Move.at(1, 1).down().drop(4))
+})
+
+test('spread stack', t => {
+  t.deepEqual(parse('111.1'), Move.at(0, 0).right().drop(1))
+  t.deepEqual(parse('231,11'), Move.at(2, 0).left().drop(1).drop(1))
+  t.deepEqual(parse('324+111'), Move.at(1, 3).up().drop(1).drop(1).drop(1))
+  t.deepEqual(parse('552-122'), Move.at(4, 1).down().drop(1).drop(2).drop(2))
+})
--- src/model/parse.js
+++ src/model/parse.js
@@ -6,31 +6,41 @@ export default function parse(ptn) {
   if (ptn.length == 2) {
     return new Place.Flat(parse_coords(ptn))
 
-  } else if (ptn.startsWith('S')) {
+  } else if (ptn.startsWith('S') || ptn.startsWith('/')) {
     return new Place.Wall(parse_coords(ptn.slice(1)))
 
-  } else if (ptn.startsWith('C')) {
+  } else if (ptn.startsWith('C') || ptn.startsWith('*')) {
     return new Place.Cap(parse_coords(ptn.slice(1)))
 
   } else if (ptn.length == 3) {
-    return new Move(parse_coords(ptn))
-      .to(ptn.slice(2))
+    return new Move(parse_coords(ptn.slice(0, 2)))
+      .to(direction(ptn.slice(2)))
       .drop(1)
 
   } else if (ptn.length == 4) {
-    return new Move(parse_coords(ptn.slice(1)))
-      .to(ptn.slice(3))
+    return new Move(parse_coords(ptn.slice(1, 3)))
+      .to(direction(ptn.slice(3)))
       .drop(parseInt(ptn.slice(0, 1)))
 
   } else {
-    const move = new Move(parse_coords(ptn.slice(1)))
-      .to(ptn.slice(3, 4))
-    move.drops = ptn.slice(4).split('').map(n => parseInt(n))
-    return move
+    return new Move(parse_coords(ptn.slice(1, 3)))
+      .to(direction(ptn.slice(3, 4)))
+      .dropping(ptn.slice(4).split('').map(n => parseInt(n)))
   }
 }
 
+function direction(symbol) {
+  if (symbol == '.') return '>'
+  if (symbol == ',') return '<'
+  return symbol
+}
+
 function parse_coords(coords) {
+  if (coords.match(/^\d+$/))
+    return new Coords(
+      parseInt(coords[0]) - 1,
+      parseInt(coords[1]) - 1)
+
   return new Coords(
     coords.charCodeAt(0) - 97,
     parseInt(coords[1]) - 1)
