f983e5eb822def0916f71e192fc589be9994f8f7
Nikolas Martens <nikolas.m@rtens.org>
2026-02-08 22:43:26 +0100

    Bot limits search time

--- modules/players/bot/spec/limits_search_time.test.js
+++ modules/players/bot/spec/limits_search_time.test.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import Bot from '../src/bot.js'
+import { setup_game } from '../../../game/spec/lib/fixture.js'
 import Game from '../../../game/src/game.js'
 
 test('timeout', async t => {
@@ -11,5 +12,23 @@ test('timeout', async t => {
   await bot.best(new Game(5).board)
   const time = new Date().getTime() - start
 
-  t.assert(time < 110, 'took ' + time)
+  t.assert(time < 120, 'took ' + time)
+})
+
+test('prevent road', async t => {
+  const game = setup_game(3, [
+    ['', 'f', 'f'],
+  ])
+
+  const bot = new class extends Bot {
+    search(board, depth, alpha, beta, timeout) {
+      return super.search(board, depth, alpha, beta, timeout)
+    }
+  }
+  bot.level = 3
+  bot.time = 10
+
+  const play = await bot.best(game.board)
+
+  t.is(play.ptn(), 'a3')
 })
--- readme.md
+++ readme.md
@@ -5,7 +5,3 @@ A simple bot to play Tak with.
 ## Goal
 
 Make all sizes fun and challenging but winnable.
-
-## Plan
-
-- limit search time
